44cddd74ffab0f058775fda83c3b05e3d943a05b
teiGustavo <gteixeiradesousa7@gmail.com>
2024-11-22 21:50:09 -0300

    🚧 feat: Menu de configurações

--- Scripts/Game/menu.gd
+++ Scripts/Game/menu.gd
@@ -9,9 +9,12 @@ const BUTTON_CLICK_SOUND: Resource = preload(
 @onready var score: Label = $Scores/Score/Value
 @onready var best_score: Label = $Scores/BestScore/Value
 @onready var play_button: TextureButton = $Buttons/PlayButton
+@onready var config_button: TextureButton = $Buttons/ConfigButton
+
 
 func _ready() -> void:
-	play_button.pressed.connect(_start_game)
+	play_button.pressed.connect(_on_play_button_pressed)
+	config_button.pressed.connect(_on_config_button_pressed)
 	update_scores()
 
 func _process(_delta: float) -> void:
@@ -21,8 +24,11 @@ func update_scores() -> void:
 	score.text = str(GameState.score)
 	best_score.text = str(GameState.best_score)
 
-func _start_game() -> void:
+func _on_play_button_pressed() -> void:
 	SoundManager.play_ui_sound(BUTTON_CLICK_SOUND)
 	update_scores()
 	GameState.reset_score()
 	get_tree().change_scene_to_file("res://Scenes/game.tscn")
+	
+func _on_config_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/config_menu.tscn")
