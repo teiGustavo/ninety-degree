7833917478f6af0e3796f6a7099b33b9c8748603
Gustavo Teixeira <gteixeiradesousa7@gmail.com>
2024-11-23 23:15:29 -0300

    :construction: feat: Adicionando as texturas corretas aos botões da tela de config

--- Scripts/Game/config_menu.gd
+++ Scripts/Game/config_menu.gd
@@ -2,15 +2,15 @@ class_name ConfigMenu
 extends Node2D
 
 
-@onready var return_button: TextureButton = $HBoxContainer/ReturnButton
-@onready var mute_music_button: TextureButton = $HBoxContainer/MuteMusicButton
-@onready var mute_sound_button: TextureButton = $HBoxContainer/MuteSoundButton
+@onready var return_button: TextureButton = $VBoxContainer/ReturnButton
+@onready var mute_sound_button: TextureButton = $VBoxContainer/HBoxContainer/MuteSoundButton
+@onready var mute_music_button: TextureButton = $VBoxContainer/HBoxContainer/MuteMusicButton
 
 
 func _ready() -> void:
 	return_button.pressed.connect(_on_return_button_pressed)
-	mute_music_button.toggled.connect(_on_mute_music_button_toggled)
 	mute_sound_button.toggled.connect(_on_mute_sound_button_toggled)
+	mute_music_button.toggled.connect(_on_mute_music_button_toggled)
 	
 func _on_return_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
