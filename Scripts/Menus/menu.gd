8828c2be48855193af3e589215e4b6b9784efa47
teiGustavo <gteixeiradesousa7@gmail.com>
2024-11-27 18:07:43 -0300

    ♻️ refactor: Mudança de nomenclatura no placar do menu inicial

--- Scripts/Menus/menu.gd
+++ Scripts/Menus/menu.gd
@@ -2,8 +2,8 @@ class_name Menu
 extends Node2D
 
 
-@onready var score: Label = $Scores/Score/Value
-@onready var best_score: Label = $Scores/BestScore/Value
+@onready var score: Label = $Scoreboards/Score/Value
+@onready var best_score: Label = $Scoreboards/BestScore/Value
 @onready var play_button: TextureButton = $Buttons/PlayButton
 @onready var config_button: TextureButton = $Buttons/ConfigButton
 
@@ -11,17 +11,14 @@ extends Node2D
 func _ready() -> void:
 	play_button.pressed.connect(_on_play_button_pressed)
 	config_button.pressed.connect(_on_config_button_pressed)
-	update_scores()
+	update_scoreboards()
 
-func _process(_delta: float) -> void:
-	pass
-
-func update_scores() -> void:
+func update_scoreboards() -> void:
 	score.text = str(GameState.score)
 	best_score.text = str(GameState.best_score)
 
 func _on_play_button_pressed() -> void:
-	update_scores()
+	#update_scoreboards()
 	GameState.reset_score()
 	get_tree().change_scene_to_file("res://Scenes/game.tscn")
 	
