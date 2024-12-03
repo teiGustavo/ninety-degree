fe46dc3db7ebbc98e2fcf4b74ba3b55e7b9f7f4b
Gustavo Teixeira <gteixeiradesousa7@gmail.com>
2024-12-03 09:03:52 -0300

    ✨ feat: Adicionando o som ao coletar power-up e ao seu efeito passar

--- Scripts/Models/power_up.gd
+++ Scripts/Models/power_up.gd
@@ -2,11 +2,20 @@ class_name PowerUp
 extends Resource
 
 
+const POWER_UP_APPLY: AudioStreamWAV = preload(
+	"res://Assets/Sounds/power_up_apply.wav"
+)
+const POWER_UP_REMOVE: AudioStreamWAV = preload(
+	"res://Assets/Sounds/power_up_remove.wav"
+)
+
 @export_range(1, 60, 0.5) var duration_in_seconds: float = 8
 @export var name: String:
 	get = _get_name
 @export var target_group: StringName = "player"
 @export var texture: CompressedTexture2D
+@export var sound_when_applied: AudioStream = POWER_UP_APPLY
+@export var sound_when_removed: AudioStream = POWER_UP_REMOVE
 
 var target: Node2D
 var allowed: bool = true
@@ -14,9 +23,11 @@ var allowed: bool = true
 
 func apply() -> void:
 	_assert_target()
+	SoundManager.play_sound(sound_when_applied)
 	
 func remove() -> void:
 	_assert_target()
+	SoundManager.play_sound(sound_when_removed)
 
 func _assert_target() -> void:
 	if not target:
